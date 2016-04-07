cdf79912cdb592fed4f4bff6bcbd6bc34fd609fa
Nikolay Nagorskiy <nikolaynag@gmail.com>
2016-04-07 19:18:37 +0300

    better concurrent counter access and more accurate bitrate calculations
    Change shared counter not on any Read, but only after buffsize amound of
    data is received or end of file reached.
    
    Calculate bitrate as measured counter delta divided by measured time
    delta (and not the requested time interval).

--- http-download-speed.go
+++ http-download-speed.go
@@ -21,30 +21,36 @@ func bignum2str(num float64) string {
 	return fmt.Sprintf("%9.3f%s", num, "Y")
 }
 
-func download(url string, counter *uint64, chunk_size int) {
+func download_loop(url string, counter *uint64, buffsize uint64) {
 	for {
+		var size uint64 = 0
 		res, err := http.Get(url)
 		if err != nil {
 			panic(err)
 		}
-		data := make([]byte, chunk_size)
+		data := make([]byte, buffsize)
 		for {
 			n, err := res.Body.Read(data)
-			if err == io.EOF {
-				break
-			}
 			if err != nil {
+				if err == io.EOF {
+					break
+				}
 				panic(err)
 			}
-			atomic.AddUint64(counter, uint64(n))
+			size += uint64(n)
+			if size >= buffsize {
+				atomic.AddUint64(counter, uint64(size))
+				size = 0
+			}
 		}
+		atomic.AddUint64(counter, uint64(size))
 	}
 }
 
 func main() {
 	routines_flag := flag.Int("routines", 1, "Number of parallel download routines")
 	interval_flag := flag.Float64("interval", 1, "Report interval in seconds")
-	buffsize_flag := flag.Int("buffsize", 500*1000, "Size of download buffers in bytes")
+	buffsize_flag := flag.Uint64("buffsize", 50*1000, "Size of download buffers in bytes")
 	url_flag := flag.String("url", "", "HTTP URL to download (REQUIRED)")
 	flag.Parse()
 	routines := *routines_flag
@@ -59,13 +65,17 @@ func main() {
 	var counter uint64 = 0
 	var prev uint64 = 0
 	for i := 0; i < routines; i++ {
-		go download(url, &counter, buffsize)
+		go download_loop(url, &counter, buffsize)
 	}
+	prev_time := time.Now()
 	fmt.Printf("Time    \tDownload speed (bit/s)\n")
 	for {
 		time.Sleep(time.Duration(interval*1e9) * time.Nanosecond)
-		fmt.Printf("%s\t%s\n", time.Now().Format("15:04:05"), bignum2str(8*float64(counter-prev)/interval))
-		prev = counter
+		now := time.Now()
+		cnt := counter
+		fmt.Printf("%s\t%s\n", now.Format("15:04:05"), bignum2str(8e9*float64(cnt-prev)/float64(now.Sub(prev_time))))
+		prev_time = now
+		prev = cnt
 	}
 
 }
